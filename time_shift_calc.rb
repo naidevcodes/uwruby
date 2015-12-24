b21d22b107278575a459e6874b36d6e4e9e2ea4a
NathanAlexander <naidledoes@gmail.com>
2015-12-24 14:36:14 -0800

    formatting

--- time_shift_calc.rb
+++ time_shift_calc.rb
@@ -1,36 +1,36 @@
 class TimeShift 
 
   def initialize time, min
-		@time = time.split(/[:\s]/)
-		@min = min
-	end
+    @time = time.split(/[:\s]/)
+    @min = min
+  end
 
-	def add_time
-		hh = @time[0].to_i
-		mm = @time[1].to_i
-		mm += @min
-		meridian = @time[2].upcase
+  def add_time
+    hh = @time[0].to_i
+    mm = @time[1].to_i
+    mm += @min
+    meridian = @time[2].upcase
 
-		xtra_hours = (@time[2].upcase == "PM") ? 720 : 0
+    xtra_hours = (@time[2].upcase == "PM") ? 720 : 0
 
-		total_min = xtra_hours + (hh * 60) + mm
+    total_min = xtra_hours + (hh * 60) + mm
 
-		meridian = 
-			(total_min.between?(720, 1439)) ? "PM" : "AM"
+    meridian = 
+      (total_min.between?(720, 1439)) ? "PM" : "AM"
 
-		while mm > 59
-			hh += 1 
-			mm -= 60   
+    while mm > 59
+      hh += 1 
+      mm -= 60   
 
-			if hh > 12
-				hh -= 12
+        if hh > 12
+          hh -= 12
 
-			end
-		end
+        end
+    end
 
-		printf "%d:%02d %s", hh, mm, meridian
+    printf "%d:%02d %s", hh, mm, meridian
 
-	end
+  end
 
 end
 
