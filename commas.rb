8d46dd2434e5b6461c2ef25a68f938fa0c2d4a2b
NathanAlexander <naidledoes@gmail.com>
2015-12-24 13:57:10 -0800

    assignment 1-inital commit

---
+++ commas.rb
@@ -0,0 +1,9 @@
+# def commas(num)
+
+#   num_wth_commas =
+#     num.to_s.reverse.split(//).each_slice(3).map {|n| n.join("").reverse}.reverse.join(",")
+# end
+
+# puts commas(-123456)
+# puts commas(7261716238172681723)
+# puts commas(9826391617236123)
